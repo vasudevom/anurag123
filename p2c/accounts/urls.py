f63417368f1c486b91a864fb4f8ac85441c77587
saraswati <payalsingh8271660141@gmail.com>
2021-06-28 18:29:22 +0530

    created_home_page created_login_page created register_page

--- p2c/accounts/urls.py
+++ p2c/accounts/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('login./', views.login, name='login'),
-    path('forgot_password/', views.forgot_password, name='forgot_password')
+    path('login/', views.login, name='login'),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('profile/',views.profile,name='profile'), # p2c-21-105
+    path('register/',views.register,name='register'), #p2c-21-105
 
 ]
