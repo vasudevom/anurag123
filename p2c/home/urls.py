8675c00bcdca82b339931f2c834f46e4cb016958
LAKSHIT SHARMA <slakshit@yahoo.com>
2021-07-01 13:32:38 +0530

    contact page integrated, updated views and urls

--- p2c/home/urls.py
+++ p2c/home/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('accounts/', views.accounts, name="accounts"),
-    path('courses', views.courses, name='courses')
-    
+    path('courses/', views.courses, name='courses'),
+    path('contact/', views.contact, name='contact '),
+    path('taxation/', views.taxation, name='taxation'),
 ]
--- p2c/home/views.py
+++ p2c/home/views.py
@@ -5,11 +5,16 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
     return render(request, 'index.html')
-  
 
 def accounts(request):
     return render(request, 'accounts.html')
 
 def courses(request):
     return render(request, 'courses.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def taxation(request):
+    return render(request, 'taxation.html')
 
